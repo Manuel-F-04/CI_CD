3a65c642eb9d2c09fb5aa3b5b919643902cebe7c
Manuel-F-04 <manuel_fam_franz@yahoo.de>
2023-11-01 17:37:48 +0100

    init

---
+++ src/main/java/org/ci_cd/Calculator.java
@@ -0,0 +1,19 @@
+package org.ci_cd;
+
+public class Calculator {
+    public static long add(long a, long b){
+        return a + b;
+    }
+
+    public static long substract(long a, long b){
+        return a - b;
+    }
+
+    public static long multiple(long a, long b){
+        return a * b;
+    }
+
+    public static long divide(long a, long b){
+        return a / b;
+    }
+}
---
+++ src/test/java/CalculatorTest.java
@@ -0,0 +1,30 @@
+import org.junit.Test;
+import org.ci_cd.*;
+import static org.junit.Assert.assertEquals;
+
+public class CalculatorTest {
+    private static long a = 10;
+    private static long b = 5;
+
+
+    @Test
+    public void testAdd(){
+        assertEquals(Calculator.add(a,b),a+b);
+    }
+
+    @Test
+    public void testSub(){
+        assertEquals(Calculator.substract(a,b),a-b);
+    }
+
+    @Test
+    public void testMult(){
+        assertEquals(Calculator.multiple(a,b),a*b);
+    }
+
+    @Test
+    public void testDiv(){
+        assertEquals(Calculator.divide(a,b),a/b);
+    }
+
+}
